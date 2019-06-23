23fd38658f98043741f8d5709138c61ba1b06031
ngohungphuc <ngohungphuc95@gmail.com>
2019-06-23 20:24:04 +0700

    add logic code

---
+++ src/FirstProject/Assets/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    //rigidbody.useGravity = false;
+    //    rigidbody.AddForce(0, 200, 500);
+    //}
+
+
+    /// <summary>
+    /// Update is called once per frame
+    /// Use FixedUpdate when we want to mess with physic not Updated
+    /// </summary>
+    void FixedUpdate()
+    {
+        rigidbody.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
