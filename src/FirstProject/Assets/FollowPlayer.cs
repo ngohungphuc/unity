f6af69f156a592d65db571347bc5a9c2d3a4a649
ngohungphuc <ngohungphuc95@gmail.com>
2019-06-23 20:49:31 +0700

    move camera

---
+++ src/FirstProject/Assets/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offSet;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offSet;
+    }
+}
