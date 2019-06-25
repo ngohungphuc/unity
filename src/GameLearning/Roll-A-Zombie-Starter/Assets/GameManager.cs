f824d1b5522f34c7992d4fcee437acd101768185
ngohungphuc <ngohungphuc95@gmail.com>
2019-06-25 22:54:12 +0700

    add scale for zombie

---
+++ src/GameLearning/Roll-A-Zombie-Starter/Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject selectedZombie;
+    public List<GameObject> zombies;
+    public Vector3 selectedSize;
+    public Vector3 defaultSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectZombie(selectedZombie); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SelectZombie(GameObject newZomeBie)
+    {
+        newZomeBie.transform.localScale = selectedSize;
+    }
+}
