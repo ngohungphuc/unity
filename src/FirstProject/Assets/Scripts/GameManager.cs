e60080064e97d91e184a14be90cd7a18611ba8ea
ngohungphuc <ngohungphuc95@gmail.com>
2019-06-25 22:10:57 +0700

    upload code

--- src/FirstProject/Assets/Scripts/GameManager.cs
+++ src/FirstProject/Assets/Scripts/GameManager.cs
@@ -4,7 +4,14 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     bool gameHasEnded = false;
+
     public float restartDelay = 1f;
+
+    public void CompleteLevel()
+    {
+
+    }
+
     public void EndGame()
     {
         if (!gameHasEnded)
