358f124a8cd7ad54ed86510a9d3b505ad592c5d1
ngohungphuc <ngohungphuc95@gmail.com>
2019-06-29 22:14:12 +0700

    move object

---
+++ src/GameLearning/Haunted Zombie Rush/Assets/Scripts/Object.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    /// <summary>
+    /// Move object from left to right
+    /// delte is the time it's took to complete the last time
+    /// </summary>
+    void Update()
+    {
+        transform.Translate(Vector3.left * (20 * Time.deltaTime));
+    }
+}
