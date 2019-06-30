d798b1ef64767fc8424c953812c212fe5c3a816b
ngohungphuc <ngohungphuc95@gmail.com>
2019-06-30 20:56:26 +0700

    use coroutines

---
+++ src/GameLearning/Haunted Zombie Rush/Assets/Scripts/Rock.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{
+    [SerializeField] Vector3 topPosition;
+    [SerializeField] Vector3 bottomPosition;
+    [SerializeField] float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Move(bottomPosition));
+    }
+
+    /// <summary>
+    /// coroutines
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Move(Vector3 target)
+    {
+        while (Mathf.Abs((target - transform.localPosition).y) > 0.20f)
+        {
+            Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+            transform.localPosition += direction * Time.deltaTime * speed;
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+
+        StartCoroutine(Move(newTarget));
+    }
+}
