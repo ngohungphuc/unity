d5eab81ab9077f0149aed3b473fa085cc4fc6ee4
ngohungphuc <ngohungphuc95@gmail.com>
2019-07-14 10:21:18 +0700

    Cloud spawner

--- src/GameLearning/Jack The Giant/Assets/Scripts/CloudSpawner.cs
+++ src/GameLearning/Jack The Giant/Assets/Scripts/CloudSpawner.cs
@@ -14,15 +14,76 @@ public class CloudSpawner : MonoBehaviour
     private GameObject[] collectables;
     private GameObject player;
 
+    void Awake()
+    {
+        controlX = 0;
+        SetMinAndMaxX();
+        CreateClouds();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetMinAndMaxX()
     {
+        Vector3 bounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
 
+        maxX = bounds.x - 0.5f;
+        minX = -bounds.x + 0.5f;
+    }
+
+    void Shuffle(GameObject[] arrayToShuffle)
+    {
+        for (int i = 0; i < arrayToShuffle.Length; i++)
+        {
+            GameObject temp = arrayToShuffle[i];
+            int random = Random.Range(i, arrayToShuffle.Length);
+            arrayToShuffle[i] = arrayToShuffle[random];
+            arrayToShuffle[random] = temp;
+        }
+    }
+
+    void CreateClouds()
+    {
+        Shuffle(clouds);
+
+        float positionY = 0f;
+
+        for (int i = 0; i < clouds.Length; i++)
+        {
+            Vector3 temp = clouds[i].transform.position;
+
+            temp.y = positionY;
+
+            if (controlX == 0)
+            {
+                temp.x = Random.Range(0.0f, maxX);
+                controlX = 1;
+            }
+            else if (controlX == 1)
+            {
+                temp.x = Random.Range(0.0f, minX);
+                controlX = 2;
+            }
+            else if (controlX == 2)
+            {
+                temp.x = Random.Range(1.0f, maxX);
+                controlX = 3;
+            }
+            else if (controlX == 3)
+            {
+                temp.x = Random.Range(-1.0f, maxX);
+                controlX = 0;
+            }
+
+            lastCloudPositionY = positionY;
+
+            clouds[i].transform.position = temp;
+
+            positionY -= distanceBetweenClouds;
+        }
     }
 }
