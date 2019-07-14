79221f9f929eebcff17365b4a953b376945f9131
ngohungphuc <ngohungphuc95@gmail.com>
2019-07-14 09:45:49 +0700

    cloud collector

---
+++ src/GameLearning/Jack The Giant/Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] clouds;
+    private float distanceBetweenClouds = 3f;
+    private float minX, maxX;
+    private float lastCloudPositionY;
+    private float controlX;
+    [SerializeField]
+    private GameObject[] collectables;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
