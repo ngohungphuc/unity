4951ae717c74a4becbb1649687adeddcaf23e5b9
ngohungphuc <ngohungphuc95@gmail.com>
2019-07-20 22:23:08 +0700

    add a script for bird

---
+++ src/GameLearning/Flappy Bird/Assets/Scripts/BirdScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    private static BirdScript instance;
+    [SerializeField]
+    private Rigidbody2D rigidbody;
+    [SerializeField]
+    private Animator anim;
+    private float fowardSpeed = 3f;
+    private float bounceSpeed = 4f;
+    private bool didFlap;
+    public bool isAlive;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        isAlive = true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (isAlive)
+        {
+            //current position of the bird
+            Vector3 temp = transform.position;
+            temp.x += fowardSpeed * Time.deltaTime;
+            transform.position = temp;
+
+            if(didFlap)
+            {
+                didFlap = false;
+                rigidbody.velocity = new Vector2(0, bounceSpeed);
+                //trigger flap animation
+                anim.SetTrigger("Flap");
+            }
+        }
+    }
+
+    public void FlapTheBird()
+    {
+        didFlap = true;
+    }
+}
