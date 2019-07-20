ead27bbc1a8d712e9b048efb53dc4d4d4e60b661
ngohungphuc <ngohungphuc95@gmail.com>
2019-07-20 23:06:34 +0700

    still need to fix camera script

--- src/GameLearning/Flappy Bird/Assets/Scripts/BirdScript.cs
+++ src/GameLearning/Flappy Bird/Assets/Scripts/BirdScript.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BirdScript : MonoBehaviour
 {
-    private static BirdScript instance;
+    public static BirdScript instance;
+    public bool isAlive;
+
     [SerializeField]
     private Rigidbody2D rigidbody;
     [SerializeField]
     private Animator anim;
+    private Button flapButton;
+
     private float fowardSpeed = 3f;
     private float bounceSpeed = 4f;
     private bool didFlap;
-    public bool isAlive;
 
     void Awake()
     {
@@ -22,6 +26,12 @@ public class BirdScript : MonoBehaviour
         }
 
         isAlive = true;
+
+        //add fuction for button
+        flapButton = GameObject.FindGameObjectWithTag("FlapButton").GetComponent<Button>();
+        flapButton.onClick.AddListener(() => FlapTheBird());
+
+        SetCameraX();
     }
 
     // Start is called before the first frame update
@@ -39,14 +49,35 @@ public class BirdScript : MonoBehaviour
             temp.x += fowardSpeed * Time.deltaTime;
             transform.position = temp;
 
-            if(didFlap)
+            if (didFlap)
             {
                 didFlap = false;
                 rigidbody.velocity = new Vector2(0, bounceSpeed);
                 //trigger flap animation
                 anim.SetTrigger("Flap");
             }
+
+            if (rigidbody.velocity.y >= 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                float angle = 0;
+                angle = Mathf.Lerp(0, -90, -rigidbody.velocity.y / 7);
+                transform.rotation = Quaternion.Euler(0, 0, angle);
+            }
         }
+    }
+
+    void SetCameraX()
+    {
+        CameraScript.offsetX = (Camera.main.transform.position.x - transform.position.x) - 1f;
+    }
+
+    public float GetPositionX()
+    {
+        return transform.position.x;
     }
 
     public void FlapTheBird()
---
+++ src/GameLearning/Flappy Bird/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public static float offsetX;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BirdScript.instance != null)
+        {
+            if (BirdScript.instance.isAlive)
+            {
+                MoveTheCamera();
+            }
+        }
+    }
+
+    void MoveTheCamera()
+    {
+        Vector3 temp = transform.position;
+        temp.x = BirdScript.instance.GetPositionX() + offsetX;
+        transform.position = temp;
+    }
+}
