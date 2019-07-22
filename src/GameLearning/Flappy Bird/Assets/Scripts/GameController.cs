042ff3fddbc40321efa5e06e326301d8383bedbd
ngohungphuc <ngohungphuc95@gmail.com>
2019-07-22 22:59:58 +0700

    add game controller

---
+++ src/GameLearning/Flappy Bird/Assets/Scripts/GameController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    private const string HIGH_SCORE = "High Score";
+    private const string SELECTED_BIRD = "Selected Bird";
+    private const string GREEN_BIRD = "Green Bird";
+
+    void Awake()
+    {
+        MakeSingleton();
+        IsGameStartFirstTime();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MakeSingleton()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+        }else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void IsGameStartFirstTime()
+    {
+        if(!PlayerPrefs.HasKey("IsGameStartFirstTime"))
+        {
+            PlayerPrefs.SetInt(HIGH_SCORE, 0);
+            PlayerPrefs.SetInt(SELECTED_BIRD, 0);
+            PlayerPrefs.SetInt(GREEN_BIRD, 0);
+            PlayerPrefs.SetInt("IsGameStartFirstTime", 0);
+        }
+    }
+
+    public void SetHighScore(int score)
+    {
+        PlayerPrefs.SetInt(HIGH_SCORE, score);
+    }
+
+    public int GetHighScore()
+    {
+        return PlayerPrefs.GetInt(HIGH_SCORE);
+    }
+
+    public void SetSelectedBird(int selectedBird)
+    {
+        PlayerPrefs.SetInt(SELECTED_BIRD, selectedBird);
+    }
+
+    public int GetSelectedBird()
+    {
+        return PlayerPrefs.GetInt(SELECTED_BIRD);
+    }
+
+    public void UnlockGreenBird()
+    {
+        PlayerPrefs.SetInt(GREEN_BIRD, 1);
+    }
+
+    public int IsGreenBirdUnlocked()
+    {
+        return PlayerPrefs.GetInt(GREEN_BIRD);
+    }
+}
