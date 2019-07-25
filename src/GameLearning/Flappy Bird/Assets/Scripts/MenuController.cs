80d1f4f3de3b661c3a3e22d284f91ffca773db05
ngohungphuc <ngohungphuc95@gmail.com>
2019-07-25 23:01:40 +0700

    add scence fader

---
+++ src/GameLearning/Flappy Bird/Assets/Scripts/MenuController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    public static MenuController instance;
+
+    [SerializeField]
+    private GameObject[] birds;
+
+    void Awake()
+    {
+        //birds[GameController.instance.GetSelectedBird()].SetActive(true);
+        MakeInstance();
+    }
+
+    private void MakeInstance()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        ScenceFader.instance.FadeIn("FlappyBird");
+    }
+}
---
+++ src/GameLearning/Flappy Bird/Assets/Scripts/ScenceFader.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenceFader : MonoBehaviour
+{
+
+    public static ScenceFader instance;
+
+    [SerializeField]
+    private GameObject fadeCanvas;
+
+    [SerializeField]
+    private Animator fadeAnim;
+
+    void Awake()
+    {
+        MakeSingleton();
+    }
+
+    void MakeSingleton()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FadeIn(string levelName)
+    {
+        StartCoroutine(FadeInAnimation(levelName));
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(FadeOutAnimation());
+    }
+
+    IEnumerator FadeInAnimation(string levelName)
+    {
+        fadeCanvas.SetActive(true);
+        fadeAnim.Play("FadeIn");
+        yield return new WaitForSeconds(.7f);
+        Application.LoadLevel(levelName);
+        FadeOut();
+    }
+
+    IEnumerator FadeOutAnimation()
+    {
+        fadeAnim.Play("FadeOut");
+        yield return new WaitForSeconds(1f);
+        fadeCanvas.SetActive(false);
+    }
+}
